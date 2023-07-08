de96d4a0355da0f3af6f2f99413594472a947f7d
Marco Antonio Velázquez Velázquez <velazquezv.marco@gmail.com>
2023-07-08 16:55:25 -0600

    First try

--- src/main/java/com/epam/rd/qa/classes/ArrayRectangles.java
+++ src/main/java/com/epam/rd/qa/classes/ArrayRectangles.java
@@ -1,39 +1,111 @@
 package com.epam.rd.qa.classes;
 
-public class ArrayRectangles {
+import net.bytebuddy.implementation.bytecode.Throw;
 
+import java.util.Arrays;
+
+public class ArrayRectangles {
+    //Private field rectangleArray - array of rectangles
+    private Rectangle[] rectangleArray;
+    //Constructor that creates a null-elements array of the given length.
+    // It should throw an IllegalArgumentException if the length is negative or zero.
     public ArrayRectangles(int size) {
         // TODO place your code here
+        if (size <= 0) {
+            throw new IllegalArgumentException("Length must not be negative or zero");
+        }
+        rectangleArray = new Rectangle[size];
+
         throw new UnsupportedOperationException();
     }
+    //Constructor that receives an arbitrary number of Rectangle objects or an array of Rectangle objects.
+    // It should throw IllegalArgumentException if the array is null or the array length is zero.
 
     public ArrayRectangles(Rectangle... rectangles) {
         // TODO place your code here
+
+        if (rectangles == null || rectangles.length == 0) {
+            throw new IllegalArgumentException("Must not be null or zero");
+        }
+        /*rectangleArray = rectangles.clone();*/
+        rectangleArray = Arrays.copyOf(rectangles, rectangles.length);
+
         throw new UnsupportedOperationException();
     }
+    //Method addRectangle that adds a Rectangle object to the array at the nearest empty place and
+    // returns true or false if there is no free space in the array.
 
     public boolean addRectangle(Rectangle rectangle) {
         // TODO place your code here
+        for (int i = 0; i < rectangleArray.length; i++) {
+            if (rectangleArray[i] == null) {
+                rectangleArray[i] = rectangle;
+                return true;
+            }
+        }
+/*        return false;*/
         throw new UnsupportedOperationException();
+
     }
 
+    //Method size that returns the actual amount of elements in the array.
+    // If addRectangle returns true, then this method should return new amount of elements.
     public int size() {
         // TODO place your code here
+            int count = 0;
+            for (Rectangle rectangle : rectangleArray) {
+                if (rectangle != null) {
+                    count++;
+                }
+            }
+            /*return count;*/
+            throw new UnsupportedOperationException();
+    }
+
+    //Method indexMaxArea that returns the index of the first rectangle with the maximum area in the array.
+    public int indexMaxArea() {
+        // TODO place your code here
+            int maxIndex = 0;
+            double maxArea = rectangleArray[0].area();
+
+            for (int i = 1; i < rectangleArray.length; i++) {
+                if (rectangleArray[i] != null && rectangleArray[i].area() > maxArea) {
+                    maxArea = rectangleArray[i].area();
+                    maxIndex = i;
+                }
+            }
+
+       /*     return maxIndex;*/
         throw new UnsupportedOperationException();
     }
 
-    public int indexMaxArea() {
-        // TODO place your code here
-        throw new UnsupportedOperationException();
-    }
+    //Method indexMinPerimeter that returns the index of the first rectangle with the minimum perimeter in the array.
 
     public int indexMinPerimeter() {
         // TODO place your code here
+        int minIndex = 0;
+        double minPerimeter = rectangleArray[0].perimeter();
+
+        for (int i = 1; i < rectangleArray.length; i++) {
+            if (rectangleArray[i] != null && rectangleArray[i].perimeter() < minPerimeter) {
+                minPerimeter = rectangleArray[i].perimeter();
+                minIndex = i;
+            }
+        }
+
+        /*return minIndex;*/
         throw new UnsupportedOperationException();
     }
-
+//Method numberSquares that returns the number of squares in the array.
     public int numberSquares() {
         // TODO place your code here
+        int count = 0;
+        for (Rectangle rectangle : rectangleArray) {
+            if (rectangle != null && rectangle.isSquare()) {
+                count++;
+            }
+        }
+        /*return count;*/
         throw new UnsupportedOperationException();
     }
 }
--- src/main/java/com/epam/rd/qa/classes/Rectangle.java
+++ src/main/java/com/epam/rd/qa/classes/Rectangle.java
@@ -1,4 +1,102 @@
 package com.epam.rd.qa.classes;
 
+import java.util.Objects;
+
 public class Rectangle {
+
+    //Private real fields sideA and sideB (sides А and В of the rectangle).
+    private double sideA;
+    private double sideB;
+
+    public Rectangle(double a, double b){//Constructor with two real
+        // parameters a and b to specify the sides of the rectangle.
+        sideA = a;
+        sideB = b;
+
+    }
+    //Constructor with a real parameter side to specify the same values for sides A and B of the rectangle.
+/*    public void sideA(double sideA){ //Constructor with a real parameter side to specify the same values for sides A and B of the rectangle.
+        this.sideA = 10;
+    }
+    public void sideB(double sideB){ //Constructor with a real parameter side to specify the same values for sides A and B of the rectangle.
+        this.sideB = 20;
+    }*/
+
+    public Rectangle(double side) {
+        sideA = side;
+        sideB = side;
+    }
+
+    //Constructor without parameters (initializes side A with 4 and side B with 3).
+    public Rectangle(){
+        sideA = 4;
+        sideB = 3;
+    }
+
+    //Method getSideA, returns value of the side А.
+
+    public double getSideA(){
+        return sideA;
+    }
+
+    //Method getSideВ, returns value of the side В.
+    public double getSideB(){
+        return sideB;
+    }
+    //Method area, calculates and returning the area value.
+    public double area(){
+        return sideA * sideB;
+    }
+    //Method perimeter, calculates and returning the perimeter value.
+    public double perimeter(){
+        return sideA * 2 + sideB * 2;
+    }
+
+    //Method isSquare, checks whether current rectangle is shape square or not.
+    // Returns true if the shape is square and false in another case.
+
+    public boolean isSquare(){
+        return sideA == sideB;
+        /*if (sideA == sideB){
+            return true;
+        }
+        else {
+            return false;
+        }*/
+    }
+
+    //Method replaceSides, swaps rectangle sides.
+    public void replaceSides(){
+        double temp = sideA;
+        sideA = sideB;
+        sideB = temp;
+    }
+
+    //Override equals and hashCode methods.
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Rectangle rectangle = (Rectangle) obj;
+        return Double.compare(rectangle.sideA, sideA) == 0 &&
+                Double.compare(rectangle.sideB, sideB) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sideA, sideB);
+    }
+
+    //Override toString method (optional, not tested).
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "sideA=" + sideA +
+                ", sideB=" + sideB +
+                '}';
+    }
 }
